4c2557147fe83c9b8977bef4421d84eeb58fc68f
Anthony Desktop <aventu6@uic.edu>
2020-11-26 16:23:20 -0600

    Fixed lighting added all models
    2 models positioned need animations, 2 others need both

---
+++ Assets/Scripts/toggleLightEmission.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toggleLightEmission : MonoBehaviour {
+
+    public Material lightMaterial;
+
+    public void disableLight() {
+        lightMaterial.DisableKeyword("_EMISSION");
+    }
+
+    public void enableLight() {
+        lightMaterial.EnableKeyword("_EMISSION");
+    }
+}

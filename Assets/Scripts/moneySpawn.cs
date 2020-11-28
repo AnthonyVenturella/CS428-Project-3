773d07e14f1fb4aa8aae5af1e7be9ae527a6df6b
Anthony Desktop <aventu6@uic.edu>
2020-11-27 22:14:10 -0600

    Project v1.0
    all deliverables completed

---
+++ Assets/Scripts/moneySpawn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moneySpawn : MonoBehaviour {
+
+    public Transform startPos;
+    public GameObject moneyModel;
+
+    // Start is called before the first frame update
+    public void spawnMoney() {
+        Instantiate(moneyModel, startPos.position, startPos.rotation);
+    }
+}
---
+++ Assets/Scripts/triggerEnter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerEnter : MonoBehaviour {
+
+    private bool inTrigger = false;
+    public Animator animator;
+    public AudioSource newSound;
+
+    public GameObject pointer;
+
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player" && inTrigger == false) {
+            Debug.Log("entered trigger volume");
+            inTrigger = true;
+
+            animator.SetTrigger("transition1");
+            newSound.Play();
+            StartCoroutine(sitdown());
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.tag == "Player" && inTrigger == true) {
+            Debug.Log("exited trigger volume");
+            inTrigger = false;
+        }
+    }
+
+    IEnumerator sitdown() {
+        yield return new WaitForSeconds(2);
+        animator.SetTrigger("transition2");
+        Debug.Log("Sit");
+    }
+}

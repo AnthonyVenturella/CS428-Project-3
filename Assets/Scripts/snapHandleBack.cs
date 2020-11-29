d4801af7507d73117bd7637849c795cbd77d7383
Anthony Desktop <aventu6@uic.edu>
2020-11-28 21:23:05 -0600

    extra cool shit
    added the CAVE environment with a VR in VR thing

---
+++ Assets/Scripts/snapHandleBack.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class snapHandleBack : MonoBehaviour {
+
+    public Transform handler;
+    public GameObject grabHandle;
+    //public Rigidbody grabHandleRB;
+
+    public void dropItem() {
+
+        grabHandle.GetComponent<Transform>().position = handler.transform.position;
+        grabHandle.GetComponent<Transform>().rotation = handler.transform.rotation;
+
+        grabHandle.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        grabHandle.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+
+
+        //grabHandleRB.velocity = Vector3.zero;
+        //grabHandleRB.angularVelocity = Vector3.zero;
+
+    }
+}
